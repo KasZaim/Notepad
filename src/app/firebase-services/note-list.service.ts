2e2c820c16f3b92565626e2c51ff4a40e61f76df
KasZaim <122385481+KasZaim@users.noreply.github.com>
2023-10-05 15:45:34 +0200

    Bug

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, doc,collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  firestore: Firestore = inject(Firestore);
+  items$;
+  items;
+
+  constructor() { 
+    this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe((list) =>{
+      list.forEach(element =>{
+        console.log(element);
+      });
+    });
+    this.items.unsubscribe();
+  }
+
+  // const itemCollection = collection(this.firestore, 'items');
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDoc(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
