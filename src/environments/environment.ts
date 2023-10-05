1c0873b6caf5876c02725c772634f0e49fc35606
KasZaim <122385481+KasZaim@users.noreply.github.com>
2023-10-05 14:36:16 +0200

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'notepad-9a02f',
+    appId: '1:28965501368:web:7863c4d019ca374715374d',
+    storageBucket: 'notepad-9a02f.appspot.com',
+    apiKey: 'AIzaSyDKmLmTfnBeFikQUn0vRpsYVz50u0oW-zQ',
+    authDomain: 'notepad-9a02f.firebaseapp.com',
+    messagingSenderId: '28965501368',
+  },
+};
