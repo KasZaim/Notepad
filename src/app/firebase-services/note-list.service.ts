293067f397b0f8a68292c3e04398cb143d6a31f2
KasZaim <122385481+KasZaim@users.noreply.github.com>
2023-10-06 12:25:37 +0200

    get Notes from Firestore

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,27 +1,59 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, doc,collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  firestore: Firestore = inject(Firestore);
-  items$;
-  items;
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
-  constructor() { 
-    this.items$ = collectionData(this.getNotesRef());
-    this.items = this.items$.subscribe((list) =>{
-      list.forEach(element =>{
-        console.log(element);
-      });
-    });
-    this.items.unsubscribe();
+  unsubTrash;
+  unsubNotes;
+  
+  firestore: Firestore = inject(Firestore);
+  
+  constructor() {
+    this.unsubTrash = this.subTrashList();
+    this.unsubNotes = this.subNotesList();
+
   }
 
-  // const itemCollection = collection(this.firestore, 'items');
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id || "",// || ist der "or Operator"
+      type: obj.type || "note",
+      title: obj.title || "",
+      content: obj.content || "",
+      marked: obj.marked || false,
+    }
+  }
+
+  ngonDestroy() {
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
 
   getNotesRef() {
     return collection(this.firestore, 'notes');
